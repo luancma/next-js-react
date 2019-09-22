cba67e56b6d98e6eb17372d95967e1d76367b5ea
Luan <luancma18@gmail.com>
2019-09-22 02:09:16 -0300

    testing SEO

--- pages/product.js
+++ pages/product.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import Link from 'next/link';
+import Head from 'next/head';
 
+import withAnalytics from '../src/hocs/withAnalytics';
 
 const Product = () => (
 
-        <div style={{ display: 'flex', justifyContent: 'space-around'}}>
+      <>
+      <Head>
+        <title>Luan Suplementos - (TOP) Whey Protein Concentrado (1KG) - Growth Supplements</title>
+      </Head>
+      <div style={{ display: 'flex', justifyContent: 'space-around'}}>
       <article style={{
         'display': 'flex',
         'flexDirection': 'column',
 
         'width': '200px'
       }}>
-        <h2>(TOP) Whey Protein Concentrado (1KG) - Growth Supplements</h2>
-        <h3>Categoria: <strong>Whey Protein</strong></h3>
+        <h1>(TOP) Whey Protein Concentrado (1KG) - Growth Supplements</h1>
+        <h2><strong>Whey Protein</strong></h2>
         <h3>Growth Supplements</h3>
 
         <img src="https://www.gsuplementos.com.br/upload/growth-layout-personalizado/produto/185/01-desk.png"  alt="Luan Suplementos - Grow"width="200"/>
@@ -22,6 +28,7 @@ const Product = () => (
       <Link href="https://www.gsuplementos.com.br/whey-protein-concentrado-1kg-growth-supplements-p985936"><a>Site oficial</a></Link>
       </article>
     </div>
+    </>
 );
 
-export default Product;
+export default withAnalytics()(Product);
