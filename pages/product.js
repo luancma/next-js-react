5d0ed65d95bbd8100a1dcea14d5ef36640fb9e68
Luan <luancma18@gmail.com>
2019-09-22 02:36:58 -0300

    teste seo max

--- pages/product.js
+++ pages/product.js
@@ -9,7 +9,8 @@ const Product = () => (
       <>
       <Head>
         <title>Luan Suplementos - (TOP) Whey Protein Concentrado (1KG) - Growth Supplements</title>
-        <meta name="description" content="Detalhes do Whey Protein Growth" />
+        <meta name="description" content="Whey Protein Growth é a proteína ideal para quem treina hipertrofia e quer ganhar massa muscular.
+        Ideal porque é um suplemento de alto valor biológico, com grande concentração de proteínas e aminoácidos e também rico em Glutamina, BCAA e Leucina." />
         <meta name="keywords" content="growth, suplementos, whey protein, bodybuilder, alimento, treino" />
       </Head>
       <div style={{ display: 'flex', justifyContent: 'space-around'}}>
@@ -23,7 +24,7 @@ const Product = () => (
         <h2><strong>Whey Protein</strong></h2>
         <h3>Growth Supplements</h3>
 
-        <img src="https://www.gsuplementos.com.br/upload/growth-layout-personalizado/produto/185/01-desk.png"  alt="Luan Suplementos - Grow"width="200"/>
+        <img src="https://www.gsuplementos.com.br/upload/growth-layout-personalizado/produto/185/01-desk.png"  alt="(TOP) Whey Protein Concentrado (1KG)" width="200"/>
       <p>Whey Protein Growth é a proteína ideal para quem treina hipertrofia e quer ganhar massa muscular.
         Ideal porque é um suplemento de alto valor biológico, com grande concentração de proteínas e aminoácidos e também rico em Glutamina, BCAA e Leucina.
       </p>
